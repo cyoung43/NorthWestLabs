5f9a327f71f36c3109f8cd3503728c65f8a27c14
cmcleod2 <56420438+cmcleod2@users.noreply.github.com>
2019-12-03 14:27:17 -0700

    Home Controller and Views

---
+++ NorthWestLabs/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthWestLabs.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult Pricing()
+        {
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            return View();
+        }
+
+        public ActionResult CreateProfile()
+        {
+            return View();
+        }
+    }
+}
