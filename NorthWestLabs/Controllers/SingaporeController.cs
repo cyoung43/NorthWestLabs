43384aaabeeaabc8d7ebd91cdc43c91eecf84824
cyoung43 <55853741+cyoung43@users.noreply.github.com>
2019-12-03 14:12:17 -0700

    Singapore

---
+++ NorthWestLabs/Controllers/SingaporeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using NorthWestLabs.DAL;
+using NorthWestLabs.Models;
+
+namespace NorthWestLabs.Controllers
+{
+    public class SingaporeController : Controller
+    {
+        private NorthWestLabsContext db = new NorthWestLabsContext();
+
+        // GET: Singapore
+        public ActionResult Index()
+        {
+            return View(db.WorkOrders.ToList());
+        }
+
+        // GET: Singapore/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrder = db.WorkOrders.Find(id);
+            if (workOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrder);
+        }
+
+        // GET: Singapore/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Singapore/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WrkOrdID,ReceivedDate,DueDate,RushOrder,comments,ReceivedBy,WOResult,ClientID")] WorkOrder workOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.WorkOrders.Add(workOrder);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(workOrder);
+        }
+
+        // GET: Singapore/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrder = db.WorkOrders.Find(id);
+            if (workOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrder);
+        }
+
+        // POST: Singapore/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WrkOrdID,ReceivedDate,DueDate,RushOrder,comments,ReceivedBy,WOResult,ClientID")] WorkOrder workOrder)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(workOrder).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(workOrder);
+        }
+
+        // GET: Singapore/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            WorkOrder workOrder = db.WorkOrders.Find(id);
+            if (workOrder == null)
+            {
+                return HttpNotFound();
+            }
+            return View(workOrder);
+        }
+
+        // POST: Singapore/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            WorkOrder workOrder = db.WorkOrders.Find(id);
+            db.WorkOrders.Remove(workOrder);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
